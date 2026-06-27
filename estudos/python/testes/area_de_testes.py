a15289205db90a338634548539e61a58e17f2e40
leandro689 <leandrobarbosa.dirceu@gmail.com>
2026-06-26 23:16:56 -0300

    volta ao projeto, refatoração de alguns itens, e revisão ortográfica e syntatica nos arquivos, foi feito também uma prova de py, consolidando conhecimentos básicos

--- estudos/python/testes/area_de_testes.py
+++ estudos/python/testes/area_de_testes.py
@@ -29,10 +29,82 @@
 
 
 
-lista = ['macaco','banana','maca']
-
-for i, item in enumerate(lista, start= 1):
-    print(i, item, sep= " - ")
 
 
+
+# numeros = [1, 2, 3, 4, 5, 6, 7]
+
+
+# i = 0
+# while i <len(numeros):
+#     if numeros[i] % 2 != 0:
+#         del numeros[i]
+#     else:
+#         i +=1
+
+# print(numeros)
+# numeros.clear()
+
+# numeros.append('lista limpa')
+# macaco = "animal"
+# numeros.insert(0, macaco)
+# print(numeros)
+
+# import random
+# import os
+
+# numero = random.randint(0, 3)
+
+# if int(numero) == 0:
+#     os.system('cls')
+#     print("Opção Rerolar")
+#     numero = random.randint(1, 3)
+#     print(numero)
+
+
+# else:
+#     os.system('cls')
+#     print(numero)
+
+#  Programa de numeros aleatorios
+# import random
+# import os
+
+# numero_aleatorio = random.randint(0,3)
+
+# if numero_aleatorio == 0:
+#     os.system('cls')
+#     print("Selecione Opção Re Rolar")
+#     numero_aleatorio = random.randint(1,3)
+#     print(numero_aleatorio)
+
+# else:
+#     os.system('cls')
+#     print(numero_aleatorio)
+
+numeros = [2, 4, 6, 8]
+
+while True:
+    entrada = input(f'digite um numero para adicionar na lista ou digite "sair" para sair da lista: ')
+
+    if entrada.isdigit():
+        entrada = int(entrada)
+        
+        if numeros[len(numeros) - 1] < entrada:
+            numeros.append(entrada)
+            print(numeros)
+            continue
+
+        for i, item_lista in enumerate(numeros):
+            if entrada > item_lista and entrada < numeros[i+1]:
+                numeros.insert(i + 1, entrada)
+        print(numeros)
+
+    elif entrada.lower() == 'sair':
+        print('saindo da lista, segue o que foi inserido até aqui: ')
+        print(numeros)
+        break
+
+    else:
+        print('entrada invalida')
 
